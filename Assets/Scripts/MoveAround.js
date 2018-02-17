147074b25a13564f9d365fa342ad3a24d491dfd2
liamseden <smailmedjadi@gmail.com>
2018-02-17 14:30:07 +0100

    Game source code folders

---
+++ Assets/Scripts/MoveAround.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var speed : float = 3.0;
+var rotateSpeed : float = 3.0;
+ 
+function Update () {
+    var controller : CharacterController = GetComponent(CharacterController);
+ 
+    // Rotate around y - axis
+    transform.Rotate(0, Input.GetAxis ("Horizontal") * rotateSpeed, 0);
+ 
+    // Move forward / backward
+    var forward : Vector3 = transform.TransformDirection(Vector3.forward);
+    var curSpeed : float = speed * Input.GetAxis ("Vertical");
+    controller.SimpleMove(forward * curSpeed);
+}
+ 
+@script RequireComponent(CharacterController)
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody  rb ;
+	public float speed;
+	private int  count;
+	public Text countText;
+	public Text winText;
+
+	void Start (){
+		rb = GetComponent <Rigidbody> ();
+		count = 0;
+		setCountText();
+		winText.text = "";
+	}
+
+	void Update (){
+		/*Jump button */
+		Vector3 jump = new Vector3 (0.0f, 10.0f, 0.0f); 
+		Vector3 down = new Vector3 (0.0f, -10.0f, 0.0f);
+
+		/*Jump touch android */
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+			rb.AddForce (jump * speed);	
+		}
+		else {
+			if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended){
+				rb.AddForce (down * speed);	
+			}
+		}
+
+
+	}
+
+
+	void FixedUpdate(){
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical	= Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);	
+	
+
+		/*Jump button */
+		Vector3 jump = new Vector3 (0.0f, 10.0f, 0.0f); 
+		Vector3 down = new Vector3 (0.0f, -10.0f, 0.0f);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rb.AddForce (jump * speed);	
+		}
+		else {
+			if (Input.GetKeyUp (KeyCode.Space)) {
+				rb.AddForce (down * speed);	
+			}
+		}
+	}
+
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive(false);		
+			count++;
+			setCountText();
+			Handheld.Vibrate();
+		}			
+	}
+
+
+	void setCountText (){
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You Win !";
+		}
+	}
+}
+
+
+
+/*
+	void Update (){
+		Vector3 dir = Vector3.zero;
+		// we assume that the device is held parallel to the ground
+		// and the Home button is in the right hand
+
+		// remap the device acceleration axis to game coordinates:
+		// 1) XY plane of the device is mapped onto XZ plane
+		// 2) rotated 90 degrees around Y axis
+		dir.x = -Input.acceleration.y;
+		dir.z = Input.acceleration.x;
+
+		// clamp acceleration vector to the unit sphere
+		if (dir.sqrMagnitude > 1)
+			dir.Normalize();
+
+		// Make it move 10 meters per second instead of 10 meters per frame...
+		dir *= Time.deltaTime;
+
+		// Move object
+		transform.Translate (dir * speed);
+	}
+	*/
